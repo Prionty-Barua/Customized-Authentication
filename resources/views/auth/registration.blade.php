2f6e35a846073b0f2ef229037fffade8cbdbc300
Prionty-Barua <priontycsebd@gmail.com>
2022-09-20 17:52:44 +0600

    login

---
+++ resources/views/auth/registration.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Customized Authentication</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4" style="margin-top:20px;">
+                <h4>Registration</h4>
+                <!-- <hr> --><br>
+                <form>
+                    <div class="form-group">
+                        <label for="name">Full name</lable>
+                            <input type="text" class="form-control" placeholder="" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</lable>
+                            <input type="email" class="form-control" placeholder="" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</lable>
+                            <input type="password" class="form-control" placeholder="" name="password">
+                    </div><br>
+                    <div class="form-group">
+                        <button class="btn btn-block btn-primary" type="submit"><a href=""></a>Create Account</button>
+                    </div><br>
+                    <div class="form-group">
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    <label for="vehicle1">I read and agree to <a href="#">terms & conditions</a></label>
+                    </div><br>
+                    <div class="form-group">
+                    <p>Already have an account?<p>
+                        <div class="form-group">
+                        <button class="btn btn-block btn-success" type="">
+                        <a href="login"></a>Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</html>
